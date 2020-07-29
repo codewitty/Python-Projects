6df7a25b8951911a52d65bd3f0a8d68926f460aa
Joshua Gomes <gomesjoshua@gmail.com>
2020-07-29 19:49:46 -0400

    changes

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -9,15 +9,22 @@ def getMeaning(w):
 	if w in data.keys():
 		return data[w]
 	elif len(get_close_matches(w, data.keys())) > 0:
-		check = input(f'Did you mean {get_close_matches(w.lower(), data.keys())[0]}? \nEnter Y if Yes or N if no.\n')
-		if check == "Y" or "y":
+		check = input(f'Did you mean {get_close_matches(w.lower(), data.keys())[0]}? \nEnter Y if Yes or N if no.\n').upper()
+		if check == "Y":
 			return data[get_close_matches(w.lower(), data.keys())[0]]
 		else:
-			print (f'No matching word found.')
+			return (f'No matching word found.')
 	else:
 		print(f'No matching word found in dictionary.')
 
 word = input("Enter word: ")
 
-print (f'{getMeaning(word)}')
+output = getMeaning(word)
 
+if type(output) == list:
+	for item in output:
+		print(item)
+else:
+	print(item)
+
+data.close()
