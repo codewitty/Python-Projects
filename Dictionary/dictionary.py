d0a09b6db2748be7dd29ca4d1ca1a62efe3138f8
Joshua Gomes <gomesjoshua@gmail.com>
2020-07-29 21:27:22 -0400

    changes

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -5,17 +5,24 @@ data = json.load(open("data.json"))
 
 
 def getMeaning(w):
-	w = w.lower()
+	wl = w.lower()
+	wu = w.upper()
 	if w in data.keys():
 		return data[w]
-	elif len(get_close_matches(w, data.keys())) > 0:
-		check = input(f'Did you mean {get_close_matches(w.lower(), data.keys())[0]}? \nEnter Y if Yes or N if no.\n').upper()
+	if wu in data.keys():
+		return data[wu]
+	if w.title() in data.keys():
+		return data[w.title()]
+	elif wl in data.keys():
+		return data[wl]
+	elif len(get_close_matches(wl, data.keys())) > 0:
+		check = input(f'Did you mean {get_close_matches(wl, data.keys())[0]}? \nEnter Y if Yes or N if no.\n').upper()
 		if check == "Y":
-			return data[get_close_matches(w.lower(), data.keys())[0]]
+			return data[get_close_matches(wl, data.keys())[0]]
 		else:
 			return (f'No matching word found.')
 	else:
-		print(f'No matching word found in dictionary.')
+		return (f'No matching word found in dictionary.')
 
 word = input("Enter word: ")
 
@@ -25,6 +32,4 @@ if type(output) == list:
 	for item in output:
 		print(item)
 else:
-	print(item)
-
-data.close()
+	print(output)
