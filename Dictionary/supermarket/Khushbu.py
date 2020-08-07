ff2f1428b60ad099163e9655d87953aec522b200
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-06 21:24:47 -0400

    new functions

--- Dictionary/supermarket/Khushbu.py
+++ Dictionary/supermarket/Khushbu.py
@@ -1,20 +1,24 @@
 import pandas
 pandas.set_option('display.max_colwidth', None)
 
-
 def main():
     df1 = pandas.read_excel("data.xlsx")
-    df2 = df1.set_index("Input")
+    df2 = df1.set_index('Input').transpose().to_dict(orient='list')
 
     exp = input("Please enter an expression: ")
-    print (f'{df2.at["Chipotl Postmates","Output"]}')
+    print (f'{df2[exp]}')
 
 if __name__ == '__main__':
     main()
 
 '''
-print(df1)
 print(df2.at["Chipotl Postmates",'Output'])
-print(df1['Output'])
 print(df2.loc["Chipotl Postmates", : ])
+data_dict = df1.to_dict()
+print(f'{data_dict}')
+V1
+    df2 = df1.set_index("Input")
+    exp = input("Please enter an expression: ")
+    print (f'{df2.at[exp,"Output"]}')
+
 '''
