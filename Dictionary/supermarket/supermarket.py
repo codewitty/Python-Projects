d1ba6d10ab90bfbef12d05e7e8d8b2246ecc4a74
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-11 23:34:26 -0400

    changes

--- Dictionary/supermarket/supermarket.py
+++ Dictionary/supermarket/supermarket.py
@@ -5,6 +5,8 @@ df2 = pandas.read_json("supermarkets.json")
 df3 = pandas.read_excel("supermarkets.xlsx")
 df4 = pandas.read_csv("supermarkets-commas.txt")
 df5 = pandas.read_csv("supermarkets-semi-colons.txt", sep = ";")
+#set ID:WQ
+
 df6 = df5.set_index("ID")
 
 print(df1)
