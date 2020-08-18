73c7ea1aae63c8d657313c9a96e6e4cbe68f3cc0
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-17 23:13:05 -0400

    changes

--- Dictionary/supermarket/supermarket.py
+++ Dictionary/supermarket/supermarket.py
@@ -1,12 +1,11 @@
 import pandas
+from geopy.geocoders import ArcGIS
 
 df1 = pandas.read_csv("supermarkets.csv")
 df2 = pandas.read_json("supermarkets.json")
 df3 = pandas.read_excel("supermarkets.xlsx")
 df4 = pandas.read_csv("supermarkets-commas.txt")
 df5 = pandas.read_csv("supermarkets-semi-colons.txt", sep = ";")
-#set ID:WQ
-
 df6 = df5.set_index("ID")
 
 print(df1)
@@ -15,3 +14,8 @@ print(df3)
 print(df4)
 print(df5)
 print(df6)
+print(type(df6))
+# Check rows and colums
+print(df6.shape)
+print(df6.shape[0])
+print(df6.shape[1])
