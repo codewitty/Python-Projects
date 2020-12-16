7ad3f685242044004f5f630dbfca2faf7dfe5778
Joshua Gomes <gomesjoshua@gmail.com>
2020-12-15 22:02:55 -0800

    paginator2

--- Automation/webscraping/paginator.py
+++ Automation/webscraping/paginator.py
@@ -16,8 +16,27 @@ for i in products:
     price = i.find('h5').text
     print(f'{count}) Product: {item} Price: {price}')
     count = count + 1
-    '''
-    quoteTags = tag[i].find_all('a',class_='tag')
-    for quote in quoteTags:
-        print(quote.text)
-    '''
+
+pages = soup.find('ul', class_='pagination')
+urls = []
+links = pages.find_all('a', class_='page-link')
+
+for link in links:
+    pageNum = int(link.text) if link.text.isdigit() else None
+    if pageNum != None:
+        x = link.get('href')
+        urls.append(x)
+
+for link in urls:
+    visitPage = url + link
+    responses = requests.get(visitPage)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    products = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
+
+    for i in products:
+        item = i.find('h4', class_='card-title').text.strip('\n')
+        price = i.find('h5').text
+        print(f'{count}) Product: {item} Price: {price}')
+        count = count + 1
+
