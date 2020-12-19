4064d8bf1a5550f9d519068ac9fcae611e07bd88
Joshua Gomes <gomesjoshua@gmail.com>
2020-12-18 23:57:11 -0800

    new projekt

--- Automation/selenium/reserveCali.py
+++ Automation/selenium/reserveCali.py
@@ -1,25 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 driver.get('https://www.reservecalifornia.com/CaliforniaWebHome/')
 
-# storing the current window handle to get back to dashbord
-main_page = driver.current_window_handle
-
-print(driver.window_handles)
-
 loginButton = driver.find_element_by_xpath('//*[@id="aLogin"]')
 loginButton.click() 
 
-# changing the handles to access login page
-for handle in driver.window_handles:
-    if handle != main_page:
-        login_page = handle
+wait = WebDriverWait(driver, 10)
 
-driver.switch_to.window(login_page)
-
-loginCred = driver.find_element_by_xpath('//*[@id="txtUserName"]')
-loginCred.send_keys('gomesjoshua@gmail.com')
+loginButton = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="txtUserName"]')))
+loginButton.send_keys('gomesjoshua@gmail.com')
 
 loginPass = driver.find_element_by_xpath('//*[@id="txtPassword"]')
 loginPass.send_keys('YLn#3G9tnbExtXw')
--- Automation/selenium/wait.py
+++ Automation/selenium/wait.py
@@ -1,16 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 
 driver.maximize_window()
-driver.get('http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
+driver.get('https://www.google.com/earth/')
 
-source = driver.find_element_by_xpath('//*[@id="DHTMLgoodies_dragableElement2"]')
-destination = driver.find_element_by_xpath('//*[@id="box103"]')
+wait = WebDriverWait(driver, 10)
 
-print(source)
-
-actions = ActionChains(driver)
-
-actions.drag_and_drop(source, destination)
+launchEarthButton = wait.until(EC.element_to_be_clickable((By.XPATH,'/html/body/header/div/nav[1]/ul[2]/li[2]/a/span/span')))
+launchEarthButton.click() 
