6a9bf74d0224991efdd223095adbde135562d6ca
Joshua Gomes <gomesjoshua@gmail.com>
2020-12-20 00:54:03 -0800

    sunrise local time

--- Automation/API/weather.py
+++ Automation/API/weather.py
@@ -1,19 +1,32 @@
 import requests
+import config
 import json
+from datetime import datetime
+from pytz import timezone
+import pytz
+from dateutil import tz
+from config import APIKEY
 
-baseUrl = 'https://api.upcitemdb.com/prod/trial/lookup'
-parameters = {'upc': '073366118238'}
-
+baseUrl = 'http://api.openweathermap.org/data/2.5/forecast'
+parameters = {'q': 'Santa Clara, CA, US', 'appid': APIKEY, 'cnt':'1', 'units':'imperial'}
 response = requests.get(baseUrl, params=parameters)
 
 content = response.content
 
 info = json.loads(content)
-item = info['items']
-itemInfo = item[0]
 
-title = itemInfo['title']
-brand = itemInfo['brand']
+cityInfo = info['city']
 
-print(title)
-print(brand)
+from_zone = tz.gettz('UTC')
+to_zone = tz.tzlocal()
+
+# Convert date unix utc string to int
+sunrise = int((cityInfo['sunrise']))
+# Use unix int timestamp to print UTC
+sun = datetime.utcfromtimestamp(sunrise)
+# sun is a naive datetime object. Make it aware and UTC
+sun_utc = sun.replace(tzinfo=from_zone)
+# use aware sun object to generate local time zone sunrise time
+rise = sun_utc.astimezone(to_zone)
+# Print sunrise in LT
+print(rise.strftime('%Y-%m-%d %H:%M:%S'))
