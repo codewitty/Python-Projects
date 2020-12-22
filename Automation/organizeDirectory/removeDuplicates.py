83f84b62251de75dc9345ae801afb71d333f0ee9
Joshua Gomes <gomesjoshua@gmail.com>
2020-12-21 18:16:18 -0800

    code cleanup

--- Automation/organizeDirectory/removeDuplicates.py
+++ Automation/organizeDirectory/removeDuplicates.py
@@ -5,28 +5,33 @@ from collections import Counter
 jpegList = []
 rawList = []
 
+def getFileName(item):
+    name = item.name
+    filePath = Path(item)
+    filetype = filePath.suffix.upper()
+    new_name = name.replace(filetype,'')
+    return new_name
+    
 def createFileList(path,emptyList):
     for item in os.scandir(path):
         if item.is_dir():
             continue
-        name = item.name
-        new_name = name[:-4]
-        emptyList.append(new_name)
+        fileName = getFileName(item)
+        emptyList.append(fileName)
 
 
 def deleteFile(rawPath,rawList,jpegList):
     res = list((Counter(rawList) - Counter(jpegList)).elements()) 
-    print(res)
     for item in os.scandir(rawPath):
         if item.is_dir():
             continue
-        name = item.name
-        raw_name = name[:-4]
+        raw_name = getFileName(item)
         if raw_name in res:
             os.remove(item)
     
 jpegPath = input("Enter jpeg path\n")
 createFileList(jpegPath, jpegList)
+print(jpegList)
 rawPath = input("Enter raw path\n")
 createFileList(rawPath, rawList)
 deleteFile(rawPath,rawList,jpegList)
