04824963038dd58379cef316fccf95091e0050dc
Joshua Gomes <gomesjoshua@gmail.com>
2020-12-26 18:02:05 -0800

    intermediate

--- webmap/nationalParkScraper.py
+++ webmap/nationalParkScraper.py
@@ -12,13 +12,20 @@ table = soup.find('table')
 table_rows = table.find_all('tr')
 
 output_rows = []
+outputs = []
 for tr in table_rows:
+    headers = tr.find_all('th')
     columns = tr.find_all('td')
     output = []
+    for header in headers:
+        outputs.append(header.text)
     for column in columns:
         output.append(column.text)
+    if not output:
+        continue
     output_rows.append(output)
+output_rows.insert(0,outputs)
 
-with open('output.csv', 'w') as csvfile:
+with open('outputH.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerows(output_rows)
