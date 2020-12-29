240eb8b65b73d47976c5a1d8f64f0eb35e0b3918
Joshua Gomes <gomesjoshua@gmail.com>
2020-12-28 16:38:30 -0800

    sort directory now works regardless of target directory location

--- Automation/organizeDirectory/test.py
+++ Automation/organizeDirectory/test.py
@@ -13,19 +13,14 @@ def pickDirectory(value):
                 return category
     return 'MISC' #If filetype doesn't exist in our dictionary
 
+
 def organizeDirectory(path):
     for item in os.scandir(path):
+        file_name = item.name
         if item.is_dir():
-            continue
-        filePath = Path(item.path)
-        fileType = filePath.suffix.lower()
-        directory = pickDirectory(fileType)
-        directoryp = os.path.join(path, directory)
-        directoryPath = Path(directoryp)
-        if directoryPath.is_dir() != True:
-            directoryPath.mkdir()
-        newpath = os.path.join(directoryp,item.name)
-        filePath.rename(newpath)
+            print(item.path)
+        else:
+            print(file_name)
 
 path = input("Enter path\n")
 organizeDirectory(path)
