ee43e107b06153fc1ee644f7531cf5f064a14922
Joshua Gomes <gomesjoshua@gmail.com>
2021-02-07 22:32:51 -0800

    personal website

--- personalWebsite/demo.py
+++ personalWebsite/demo.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello you gorgeous Flask developer"
+    return render_template("index.html")
 
 @app.route('/about/')
 def about():
-    return "This is the start of something new"
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
