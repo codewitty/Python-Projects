29b6a6a1dd9c8f09dc8a0db20cbd7429131d7c6e
Joshua Gomes <gomesjoshua@gmail.com>
2021-02-11 22:39:12 -0800

    added nps to site

--- personalWebsite/demo.py
+++ personalWebsite/demo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -7,9 +7,20 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/about/')
-def about():
-    return render_template("about.html")
+@app.route('/contact/')
+def contact():
+    return render_template("ContactMe.html")
+
+@app.route('/np/')
+def np():
+    return render_template("NationalParksMap.html")
+
+"""
+@app.route('/resume/')
+def resume():
+    path = "Joshua_Gomes_Resume.pdf"
+    return send_file(path, as_attachment=True)
+"""
 
 if __name__ == "__main__":
     app.run(debug=True)
