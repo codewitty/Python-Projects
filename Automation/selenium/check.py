28beb9e771e68991eff4841a6a0a8de43d597022
Joshua Gomes <gomesjoshua@gmail.com>
2021-02-24 02:23:57 -0800

    changes

---
+++ Automation/selenium/check.py
@@ -0,0 +1,5 @@
+
+int_list = []
+int_list.append([str(num).zfill(5) for num in range(100000)])
+
+print(int_list)
--- Automation/selenium/reserveCali.py
+++ Automation/selenium/reserveCali.py
@@ -3,19 +3,41 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome()
-driver.get('https://www.reservecalifornia.com/CaliforniaWebHome/')
+int_list = []
 
-loginButton = driver.find_element_by_xpath('//*[@id="aLogin"]')
+for num in range(100000):
+    nums = str(num).zfill(5)
+    int_list.append(nums)
+
+driver = webdriver.Chrome()
+driver.get('https://tools.usps.com/zip-code-lookup.htm?citybyzipcode')
+findZipButton = driver.find_element_by_xpath('//*[@id="tZip"]')
+
+
+findZipButton.click()
+findZipButton.send_keys('00501')
+loginButton = driver.find_element_by_xpath('//*[@id="cities-by-zip-code"]')
 loginButton.click() 
+#outputButton = driver.find_element_by_xpath('//*[@id="cityByZipDiv" and @class="row col-md-5 col-sm-12 col-xs-12 recommended-cities"]').getText()
+outputButton = driver.find_element_by_xpath('//*[@id="cityByZipDiv"]')
+text_output = driver.find_element_by_class_name("row col-md-5 col-sm-12 col-xs-12 recommended-cities")
+print(text_output)
+
+"""
+
+for i in int_list: 
+    findZipButton.click()
+    findZipButton.send_keys(i)
+    loginButton = driver.find_element_by_xpath('//*[@id="cities-by-zip-code"]')
+    loginButton.click() 
+    findZipButton.clear()
 
 wait = WebDriverWait(driver, 10)
 
-loginButton = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="txtUserName"]')))
 loginButton.send_keys('gomesjoshua@gmail.com')
 
 loginPass = driver.find_element_by_xpath('//*[@id="txtPassword"]')
-loginPass.send_keys('YLn#3G9tnbExtXw')
 
 loginButton2 = driver.find_element_by_xpath('//*[@id="divOnlyLogin"]')
 loginButton2.click() 
+"""
